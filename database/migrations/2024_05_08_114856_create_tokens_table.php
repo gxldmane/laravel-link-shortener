3590294ef2a3ed6fb970aa13515c2a20443dc975
gxldmane <timofey.hodotaev@yandex.ru>
2024-05-08 16:52:18 +0500

    init

---
+++ database/migrations/2024_05_08_114856_create_tokens_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('original_link');
+            $table->string('short_link');
+            $table->unsignedBigInteger('view_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tokens');
+    }
+};
